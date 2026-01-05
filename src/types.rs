a40ab5c87e3e55000d26ea75a83f8d0ae583503f
Maxim Petrov <mmrmaximuzz@gmail.com>
2026-01-05 15:42:35 +0300

    introduce pmppt types module

--- src/types.rs
+++ src/types.rs
@@ -13,9 +13,3 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-pub mod agent;
-pub mod common;
-pub mod controller;
-pub mod plotters;
-pub mod types;
